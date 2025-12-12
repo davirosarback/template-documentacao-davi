c549fb7a366d820619f233650284ed9ce42726cb
davirosarback <82427024+davirosarback@users.noreply.github.com>
2025-12-12 19:07:09 -0300

    firsto commit

---
+++ theme.config.jsx
@@ -0,0 +1,16 @@
+export default {
+  logo: <span>Minha Doc Nextra</span>,
+  project: {
+    link: 'https://github.com/meu-usuario/meu-docs'
+  },
+  docsRepositoryBase: 'https://github.com/meu-usuario/meu-docs/blob/main/',
+  search: {
+    placeholder: 'Buscar...'
+  },
+  footer: {
+    text: `MIT ${new Date().getFullYear()} © Minha Empresa`
+  },
+  i18n: [
+  { locale: 'pt', name: 'Português' },
+  { locale: 'es', name: 'Espanhol' }]
+}
